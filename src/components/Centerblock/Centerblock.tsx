8cc27220a292cebe79e1dddc3ae6b375b4c6f9bf
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2026-04-14 18:46:20 +0300

    h2

--- src/components/Centerblock/Centerblock.tsx
+++ src/components/Centerblock/Centerblock.tsx
@@ -1,4 +1,5 @@
 import styles from './centerblock.module.css';
+import Search from '../Search/Search';
 import Filter from '../Filter/Filter';
 import PlaylistTitle from '../PlaylistTitle/PlaylistTitle';
 import PlaylistTracks from '../PlaylistTracks/PlaylistTracks';
@@ -7,17 +8,7 @@ import PlaylistTracks from '../PlaylistTracks/PlaylistTracks';
 export default function Centerblock() {
   return (
     <div className={styles.centerblock}>
-      <div className={styles.centerblock__search}>
-        <svg className={styles.search__svg}>
-          <use xlinkHref="/img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className={styles.search__text}
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
+      <Search/>
       <h2 className={styles.centerblock__h2}>Треки</h2>
       <Filter />
       <div className={styles.centerblock__content}>
--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,13 +1,41 @@
+'use client'
+
 import styles from './filter.module.css';
+import FilterItem from '../FilterItem/FilterItem';
+import { useState } from 'react';
 
 
 export default function Filter() {
+  const [isOpen, setIsOpen] = useState("");
+  const [activeFilter, setActiveFilter] = useState("");
+
+  const onOpenDropdownList = (title: string) => {
+    setIsOpen(title === isOpen ? "" : title); // закрыть список, если он уже открыт
+    setActiveFilter(title);
+    // console.log("Открыть список: ", title);
+  };
+
   return (
     <div className={styles.centerblock__filter}>
       <div className={styles.filter__title}>Искать по:</div>
-      <div className={styles.filter__button}>исполнителю</div>
-      <div className={styles.filter__button}>году выпуска</div>
-      <div className={styles.filter__button}>жанру</div>
+      <FilterItem
+        title="исполнителю"
+        onClick={() => onOpenDropdownList("исполнителю")}
+        isOpen={isOpen === "исполнителю"}
+        activeFilter={activeFilter}
+        />
+      <FilterItem
+        title="году выпуска"
+        onClick={() => onOpenDropdownList("году выпуска")}
+        isOpen={isOpen === "году выпуска"}
+        activeFilter={activeFilter}
+        />
+      <FilterItem
+        title="жанру"
+        onClick={() => onOpenDropdownList("жанру")}
+        isOpen={isOpen === "жанру"}
+        activeFilter={activeFilter}
+      />
     </div>
   )
 }
