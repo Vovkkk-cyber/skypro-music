27fb24df336949aa410ab45dee74e52edb85d5ce
Vovkkk-cyber <vov.blindowscky@yandex.ru>
2026-07-01 18:47:54 +0300

    h7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from 'next/font/google';
 import './globals.css';
 import ReduxProvider from '@/store/ReduxProvider';
 
+
 const montserrat = Montserrat({
   variable: '--font-montserrat',
   subsets: ['latin'],
